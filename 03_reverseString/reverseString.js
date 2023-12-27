df20d4876e4d59c02aba767590217853cba658ca
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 14:18:11 -0800

    Attempt to use 'for' loop to reverse string via concatenation.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let new_string = '';
+    for(let i  = 0; i < string.length(); i++){
+        new_string = string.at(i).concat(new_string);
+    }
+    return new_string;
 };
 
 // Do not edit below this line
