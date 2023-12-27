574c3b96d9f38b8e2be64eb1875ed3ecf59cb8bc
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 14:19:56 -0800

    Change syntax of length from C++ to JavaScript.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function(string) {
     let new_string = '';
-    for(let i  = 0; i < string.length(); i++){
+    for(let i  = 0; i < string.length; i++){
         new_string = string.at(i).concat(new_string);
     }
     return new_string;
