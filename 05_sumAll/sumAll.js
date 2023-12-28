41ad4c7cad6c661cb4d051450ee19b1486a3757c
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 17:22:51 -0800

    Add ERROR return for non-integer values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,16 @@
 const sumAll = function(number1, number2) {
     let sum = 0;
     
-    if(number1 < 0 || number2 < 0) return 'ERROR';
+    if(number1 < 0 || number2 < 0 ||
+    !Number.isInteger(number1) || !Number.isInteger(number2)) return 'ERROR';
+    
     //If the first number is larger, switch the two
     if(number1 > number2){
         let temp = number1;
         number1 = number2;
         number2 = temp;
     }
+    
     for(let i = number1; i <= number2; i++){
         sum += i;
     }
