ac6e387adec191192119db9b5fff2e2bec780f67
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 17:16:14 -0800

    Add a check for ensuring the smaller number is first.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
 const sumAll = function(number1, number2) {
     let sum = 0;
+    
+    //If the first number is larger, switch the two
+    if(number1 > number2){
+        let temp = number1;
+        number1 = number2;
+        number2 = number1;
+    }
     for(let i = number1; i <= number2; i++){
         sum += i;
     }
