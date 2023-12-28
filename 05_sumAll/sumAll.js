64d6786fc89fefc041e410494890a351664ea1b0
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 17:13:25 -0800

    Adjust for loop to include last number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(number1, number2) {
     let sum = 0;
-    for(let i = number1; i < number2; i++){
+    for(let i = number1; i <= number2; i++){
         sum += i;
     }
     return sum;
