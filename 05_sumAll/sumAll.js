a79e2d4f7d6d2e5b5cdad84ba4cd12c5fa5322cb
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 17:12:25 -0800

    Use for loop to add numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let sum = 0;
+    for(let i = number1; i < number2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
