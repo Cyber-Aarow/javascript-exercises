15d6d38c1d706d13679e949dac4f535f964a6218
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 16:55:12 -0800

    Repeat for second element.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
-const removeFromArray = function(array, value) {
+const removeFromArray = function(array, value, value2) {
     let index = array.indexOf(value);
     if(index != -1){
         array.splice(index, 1);
-        return array;
     }
+    let index2 = array.indexOf(value2);
+    if(index2 != -1){
+        array.splice(index2, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
