2177cb4523fb58d959eff4920723d25ddb130449
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2023-12-27 17:19:19 -0800

    Add ERROR return for negative values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function(number1, number2) {
     let sum = 0;
     
+    if(number1 < 0 || number2 < 0) return 'ERROR';
     //If the first number is larger, switch the two
     if(number1 > number2){
         let temp = number1;
