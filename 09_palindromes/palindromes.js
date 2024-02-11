4a283c08daeb9c51ed4501e3984ad7aaa31fe32b
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-02-10 16:47:02 -0800

    Try .split.reduce method.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversed_string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    reversed_string = reversed_string.split("").reduce((char1, char2) => char2 + char1, '');
+    return reversed_string === string;
 };
 
 // Do not edit below this line
