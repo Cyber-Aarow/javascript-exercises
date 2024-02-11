0e9204186d54f9affd6f76dec2b84fc50c293db2
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-02-10 16:49:55 -0800

    Test reversed string against already-processed spaceless string instead of original.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,6 @@
 const palindromes = function (string) {
-    let reversed_string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    reversed_string = reversed_string.split('').reverse().join('');
-    return reversed_string === string;
+    let spaceless_string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    return spaceless_string === spaceless_string.split('').reverse().join('');
 };
 
 // Do not edit below this line
