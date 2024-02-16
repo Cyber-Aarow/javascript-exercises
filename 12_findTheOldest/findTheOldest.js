db5494418b61ba603ff212a5a12f55494171afe4
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-02-15 20:00:39 -0800

    Use a for loop to find each person's age and compare it to an oldest_person and oldest_age.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest_person = {};
+    let age = 0;
+    let oldest_age = 0;
+    for(person of people){
+        age = person.yearOfDeath - person.yearOfBirth;
+        if(age > oldest_age){
+            oldest_age = age;
+            oldest_person = person;
+        }
+    }
+    return oldest_person;  
 };
 
 // Do not edit below this line
